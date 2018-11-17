261fc65605b4a892e20b41afcd83054ea49d35ed
Collin Stewart <cstewa91@gmail.com>
2018-11-17 00:17:33 -0800

    Completed getting list data from server

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import 'materialize-css/dist/css/materialize.min.css';
 import 'materialize-css/dist/js/materialize';
+import { Route } from 'react-router-dom';
+import List from './list'
 
 const App = () => (
     <div className="container">
-        <h1 className="center">To Do List</h1>
-        <p className="center grey-text">now with Redux</p>
+        <Route exact path="/" component={List} />
     </div>
 );
 
