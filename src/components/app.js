1a554705194512f31fce48c03c9a4c1ccde0912c
Collin Stewart <cstewa91@gmail.com>
2018-11-17 00:33:46 -0800

    add item

--- src/components/app.js
+++ src/components/app.js
@@ -3,10 +3,12 @@ import 'materialize-css/dist/css/materialize.min.css';
 import 'materialize-css/dist/js/materialize';
 import { Route } from 'react-router-dom';
 import List from './list'
+import AddItem from './add_item'
 
 const App = () => (
     <div className="container">
         <Route exact path="/" component={List} />
+        <Route path="/add-item" component={AddItem}></Route>
     </div>
 );
 
